8c1859e8a7222bd58910e3cf5ad13a1144f9b124
hijiki51 <hibiki0719euph@gmail.com>
2023-06-14 09:48:26 +0900

    fix: 🐛 fix host

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ ${url}`;
   return;
 });
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: 3000, host: "localhost" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
